dbfd1f1ddc8b5246810c012b8c68129bc8da714f
sthancock <steven.hancock@ed.ac.uk>
2021-05-16 16:31:51 +0100

    Initial 3-phase draft complete.

--- throttle_test/throttle_test.ino
+++ throttle_test/throttle_test.ino
@@ -5,28 +5,203 @@
 /*# TBAG 2021         #*/
 /*#####################*/
 
+#define DEBUG
 
 // set all the pins
-const unsigned char RpPin = 12;  // red positive pin
-const unsigned char RnPin = 6;   // red negative pin
-const unsigned char GpPin = 11;  // green
-const unsigned char GnPin = 5;   // green
-const unsigned char BpPin = 10;  // blue
-const unsigned char BnPin = 4;   // blue
-const unsigned char throt1Pin = A5; // input for throttle 1
+const unsigned char RpPin=12;     // red positive pin
+const unsigned char RnPin=6;      // red negative pin
+const unsigned char GpPin=11;     // green
+const unsigned char GnPin=5;      // green
+const unsigned char BpPin=10;     // blue
+const unsigned char BnPin=4;      // blue
+const unsigned char throt1Pin=A5; // input for throttle 1
 
 class procedures{
-  const float maxRPM=4000;      // maximum RPM for tachometers
-  const float minThrotV=1024.0; // maximum throttle input voltage
+  public:
+    // methods
+    void setup();
+    void readInputs();
+    void determineState();
+    void writeState();
+
+  private:
+    // methods
+    void setPhase(float);
+    // fixed constants
+    const float maxRPM=4000;      // maximum RPM for tachometers
+
+    // throttle controls
+    float throtPos1;  // throttle position, in %
+    char cockPos;     // fuel cock position. On/off
+  
+    // throttle internals
+    const float tRate=100.0/5.0;       // throttle change rate, in % per second
+    float tim;         // time now
+    char rDir;         // red tacho directions
+    char gDir;         // green tacho directions
+    char bDir;         // blue tacho directions
+  
+    // throttle outputs
+    float rpm1;        // tachometer RPM, in %
+    float temp1;       // exhaust temperature
   
 }proc;
 
-void setup() {
-  // put your setup code here, to run once:
+/*##############################*/
+/*determine tachometer phases*/
 
+void procedures::setPhase(float thisTime)
+{
+  float l=0,fracL=0;
+  const float third=1.0/3.0;
+
+  l=rpm1/maxRPM;   /*wavelength in seconds*/
+  fracL=(tim/l)-(float)(int)(tim/l);     /* fraction of a wavelength*/
+
+  if(fracL<0.5)rDir=1;   // red
+  else         rDir=-1;
+  if((fracL>=third)&&(fracL<(third+0.5)))gDir=1;  // green
+  else                                   gDir=-1;
+  if((fracL>(2.0*third))||(fracL<(2.0*third-0.5)))bDir=1; // blue
+  else                                             bDir=-1;
+
+  return;
 }
+
+/*##############################*/
+/*internal setup*/
+
+void procedures::setup()
+{
+  // inputs
+  throtPos1=0.0; // starts with throttle closed
+  cockPos=1;     // hard-coded open for now, as now switch
+
+  // internals
+  
+  // outputs
+  rpm1=0.0;     // everything off
+  temp1=0.0;
+
+  return;
+}
+
+/*##############################*/
+/*read input controls*/
+
+void procedures::readInputs()
+{
+  //throttle
+  throtPos1=(float)map(analogRead(throt1Pin), 0, 1024, 0, 100);
+
+  return;
+}
+
+/*##############################*/
+/*adjust the states*/
+
+void procedures::determineState()
+{
+  float dFuel=0;
+  float thisTime=0,dTime=0;
+
+  // time change since last call?
+  thisTime=micros()/1000.0;
+  dTime=thisTime-tim;
+
+  // determine delta fuel
+  if(cockPos)dFuel=throtPos1-rpm1;
+  else       dFuel=-1.0*rpm1;
+
+  // update rpm and temperatures
+  rpm1+=dFuel*tRate*dTime;
+  if(rpm1>50.0)temp1=50.0;
+  else         temp1=0.0;
+
+  // update time
+  tim=thisTime;
+  return;
+}
+
+
+/*##############################*/
+/*write state*/
+
+void procedures::writeState()
+{
+  // determine phase for RPM
+
+  // is this a change?
+  // update this so a digitalWrite is only done for a direction change
+
+  //eventually this should be replaced with arrays
+  if(rDir>0){  // red phase
+    digitalWrite(RpPin,HIGH);
+    digitalWrite(RnPin,LOW);
+  }else if(rDir<0){
+    digitalWrite(RpPin,LOW);
+    digitalWrite(RnPin,HIGH);
+  }else{
+    digitalWrite(RpPin,LOW);
+    digitalWrite(RnPin,LOW);
+  }
+  if(gDir>0){  // green phase
+    digitalWrite(GpPin,HIGH);
+    digitalWrite(GnPin,LOW);
+  }else if(gDir<0){
+    digitalWrite(GpPin,LOW);
+    digitalWrite(GnPin,HIGH);
+  }else{
+    digitalWrite(GpPin,LOW);
+    digitalWrite(GnPin,LOW);
+  }
+  if(bDir>0){  // green phase
+    digitalWrite(BpPin,HIGH);
+    digitalWrite(BnPin,LOW);
+  }else if(bDir<0){
+    digitalWrite(BpPin,LOW);
+    digitalWrite(BnPin,HIGH);
+  }else{
+    digitalWrite(BpPin,LOW);
+    digitalWrite(BnPin,LOW);
+  }
+  return;
+}
+
+/*##############################*/
+/*set things up*/
+
+void setup() {
+  // allocate pins
+  pinMode(RpPin, OUTPUT);
+  pinMode(RnPin, OUTPUT);
+  pinMode(GpPin, OUTPUT);
+  pinMode(GnPin, OUTPUT);
+  pinMode(BpPin, OUTPUT);
+  pinMode(BnPin, OUTPUT);
+  pinMode(throt1Pin, INPUT);
+
+  // set positions etc.
+  proc.setup();
+  
+}
+
+/*##############################*/
+/*main loop*/
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+  // read controls
+  proc.readInputs();
+
+  // determine state
+  proc.determineState();
+
+  // write outputs
+  proc.writeState();
+  
+}/*main loop*/
+
+/*the end*/
+/*##############################*/
+

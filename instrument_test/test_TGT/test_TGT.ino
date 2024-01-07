af683938609c2cfa0f9b0b7c80553946a907c620
sthancock <steven.hancock@ed.ac.uk>
2024-01-07 13:39:53 +0000

    TGT test added.

---
+++ instrument_test/test_TGT/test_TGT.ino
@@ -0,0 +1,41 @@
+
+/*#######################*/
+/*# Program to test a   #*/
+/*# buccaneer TGT gauge #*/
+/*# TBAG 2024           #*/
+/*#######################*/
+
+
+
+/*#####################################*/
+/*pin assignments*/
+
+#define VARPIN A1  /* potentiometer in*/
+#define TGTPIN 8   /*PWM out*/
+
+
+/*#####################################*/
+/*variables*/
+
+//float varPos;   /*potentiometer position. 0-1*/
+
+
+/*#####################################*/
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(VARPIN, INPUT);
+  pinMode(TGTPIN,OUTPUT);
+}
+
+
+/*#####################################*/
+/*the main loop*/
+
+void loop() {
+  //varPos=(float)map(analogRead(VARPIN),0,1024,0,1.0);
+
+  return;
+}
+
+/*#####################################*/
